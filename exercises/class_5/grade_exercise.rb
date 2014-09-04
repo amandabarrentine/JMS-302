45d05456208171e1e32d73aaa50ff2c8b88ce301
Amanda Barrentine <amanda_barrentine@yahoo.com>
2014-09-04 10:37:48 -0400

    Adding homework

---
+++ exercises/class_5/grade_exercise.rb
@@ -0,0 +1,10 @@
+puts "Please enter five grades."
+grades = [5]
+grade = gets.chomp.to_i
+grades.push(grade)
+puts grades
+    
+    #average_one = grades[0] + grades[1]
+    #puts average_one
+    #average_two = grades[2] + grades[3] + grade[4]
+    #puts average_two
---
+++ exercises/class_5/name_exercise.rb
@@ -0,0 +1,14 @@
+puts 'What is your first name?'
+first_name = gets.chomp
+puts 'What is your middle name?'
+middle_name = gets.chomp
+puts 'What is your last name?'
+last_name = gets.chomp
+
+puts first_name.reverse
+puts middle_name.reverse
+puts last_name.reverse
+
+name_length = first_name.length + middle_name.length + last_name.length
+puts 'Did you know there are ' + name_length.to_s + ' characters'
+puts 'in your name, ' + first_name + ' ' + middle_name + ' '+ last_name + ' ?'
